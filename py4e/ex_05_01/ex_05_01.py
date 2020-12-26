847fde2ea7f5a21ba9489e0752679f6c1d5c7445
paulapivat <paul.apivat@gmail.com>
2020-12-26 23:26:09 +0700

    add ex_05_01

---
+++ py4e/ex_05_01/ex_05_01.py
@@ -0,0 +1,18 @@
+num = 0
+total = 0.0
+
+while True:
+    sval = input("Enter a number: ")
+    if sval == "done":
+        break
+    try:
+        fval = float(sval)
+    except:
+        print('Invalid Input')
+        continue
+    # print(fval)
+    num += 1
+    total = total + fval
+
+#print('ALL DONE')
+print(f'total: {total}, num: {num}, avg: {total/num}')
