930280c4d6671074bc8e27787fe0c63179c3d437
paulapivat <paul.apivat@gmail.com>
2021-01-16 22:09:28 +0700

    edit comment urllib_words.py

--- py4e/urllib_words.py
+++ py4e/urllib_words.py
@@ -12,7 +12,7 @@ fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 counts = dict()
 
 for line in fhand:
-    words = line.decode().split()   # .split() vs .strip()
+    words = line.decode().split()   # .split() vs .strip() - count words vs letters
     for word in words:
         counts[word] = counts.get(word, 0) + 1
 print(counts)
