9fcbb317abdc17c21d5d9c0629d8246605991e13
paulapivat <paul.apivat@gmail.com>
2021-01-16 22:05:39 +0700

    edit urllib_words.py to change .split() to .strip()

--- py4e/urllib_words.py
+++ py4e/urllib_words.py
@@ -12,7 +12,7 @@ fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 counts = dict()
 
 for line in fhand:
-    words = line.decode().strip()
+    words = line.decode().split()   # .split() vs .strip()
     for word in words:
         counts[word] = counts.get(word, 0) + 1
 print(counts)
