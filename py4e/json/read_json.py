91299cf748087257939ba44f9b1364809cbc29ca
paulapivat <paul.apivat@gmail.com>
2021-01-18 21:42:47 +0700

    create read_json2.py

--- py4e/json/read_json.py
+++ py4e/json/read_json.py
@@ -6,10 +6,32 @@ import json
 with open('your_posts_1.json') as file:
     data = json.load(file)
 
+len(data)  # 2166 dictionaries inside this list
+
 # print first dictionary in 'data' list
 for k, v in data[0].items():
     if k == 'data':
         # nested conditional for-loop to get to 'post' content
         for k_i, v_i in v[0].items():
             if k_i == 'post':
+                post = v_i
                 print(v_i)
+
+post_split = post.split()
+
+# initialize new dictionary to count words
+counts = dict()
+for word in post_split:
+    counts[word] = counts.get(word, 0) + 1
+print("\nA count of all the words: \n", counts)
+
+# sort dictionary by keys
+sorted_dict = dict()
+
+sorted_keys = sorted(counts, key=counts.get)
+
+for w in sorted_keys:
+    sorted_dict[w] = counts[w]
+print("\nSorted dictionary: \n", sorted_dict)
+
+print("\nNOW THE FULL DATA\n")
---
+++ py4e/json/read_json2.py
@@ -0,0 +1,30 @@
+import json
+
+# load json into python, assign to 'data'
+with open('your_posts_1.json') as file:
+    data = json.load(file)
+
+len(data)  # 2166 dictionaries inside this list
+
+
+# create empty list
+empty_lst = []
+
+# create counter
+count = 0
+
+# multiple nested loops to store all post in empty list
+for dct in data:
+    for k, v in dct.items():
+        if k == 'data':
+            vee = v
+            count += 1           # helping to diagnose a problem
+            print(vee, count)
+            #print("this is vee:", vee)
+            for k_i, v_i in vee[0].items():  # <-- Bug
+                if k_i == 'post':
+                    vee2 = v_i
+                    #print("this is vee2:", vee2)
+                    empty_lst.append(vee2)
+
+print("This is the empty list:", empty_lst)
