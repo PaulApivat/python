c6c10630f702b57ccf5041547d5b96fc36bbe867
paulapivat <paul.apivat@gmail.com>
2021-01-22 00:20:31 +0700

    add stems and lemmas to normalization

--- py4e/json/read_json2.py
+++ py4e/json/read_json2.py
@@ -186,3 +186,13 @@ def normalize(words):
 
 words = normalize(flat_new_lst)
 print(words)
+
+
+def stem_and_lemmatize(words):
+    stems = stem_words(words)
+    lemmas = lemmatize_verbs(words)
+    return stems, lemmas
+
+stems, lemmas = stem_and_lemmatize(words)
+print('Stemmed:\n', stems)
+print('\nLemmatized:\n', lemmas)
