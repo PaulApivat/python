9ff877c99491b0d897bca0307c0205f57c860716
paulapivat <paul.apivat@gmail.com>
2021-01-22 22:08:46 +0700

    add frequency graph and plot

--- py4e/json/text_analysis.py
+++ py4e/json/text_analysis.py
@@ -1,10 +1,12 @@
 from nltk.stem import LancasterStemmer, WordNetLemmatizer
 from nltk.corpus import stopwords
+from nltk.probability import FreqDist
 import re
 import unicodedata
 import nltk
 import json
 import inflect
+import matplotlib.pyplot as plt
 
 # load json into python, assign to 'data'
 with open('your_posts_1.json') as file:
@@ -38,6 +40,41 @@ for dct in data:
 print("This is the empty list: ", empty_lst)
 print("\nLength of list: ", len(empty_lst))
 
-# Sample Tokenizations
+##### Sample Tokenizations ######
+
 # sample_words = nltk.word_tokenize(empty_lst[0])        len: 96
 # sample_sentence = nltk.sent_tokenize(empty_lst[0])     len: 5
+
+##### Tokenize Facebook Posts #####
+
+# - list of list, len: 1762 (each list contain individual words)
+nested_word_token = [nltk.word_tokenize(word) for word in empty_lst]
+
+# - list of list, len: 1762 (each list contain sentences)
+nested_sent_token = [nltk.sent_tokenize(lst) for lst in empty_lst]
+
+print("Nested Word Tokens: ", len(nested_word_token))
+print("Nested Sentence Token: ", len(nested_sent_token))
+
+### FLATTEN LIST ###
+
+flat_word_token = [item for sublist in nested_word_token for item in sublist]
+print("Flatten word token: ", len(flat_word_token))
+
+flat_sent_token = [item for sublist in nested_sent_token for item in sublist]
+print("Flatten sentence token: ", len(flat_sent_token))
+
+# Find Frequency Distribution
+
+# Find frequency of words
+fdist_word = FreqDist(flat_word_token)
+
+# Find frequency of sentence
+fdist_sent = FreqDist(flat_sent_token)
+
+# fdist_word.most_common(50)
+
+# Plot Frequency Graph
+
+# fdist_word.plot(50)
+# fdist_sent.most_common(10)
