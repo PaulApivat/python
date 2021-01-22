f7561676ca52e450032aad0ef04d2fd0bff83541
paulapivat <paul.apivat@gmail.com>
2021-01-22 23:01:39 +0700

    complete normalization and frequency distribution plot

--- py4e/json/text_analysis.py
+++ py4e/json/text_analysis.py
@@ -64,17 +64,105 @@ print("Flatten word token: ", len(flat_word_token))
 flat_sent_token = [item for sublist in nested_sent_token for item in sublist]
 print("Flatten sentence token: ", len(flat_sent_token))
 
-# Find Frequency Distribution
+
+# Remove Non-ASCII
+
+
+def remove_non_ascii(words):
+    """Remove non-ASCII character from List of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = unicodedata.normalize('NFKD', word).encode(
+            'ascii', 'ignore').decode('utf-8', 'ignore')
+        new_words.append(new_word)
+    return new_words
+
+# To LowerCase
+
+
+def to_lowercase(words):
+    """Convert all characters to lowercase from List of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = word.lower()
+        new_words.append(new_word)
+    return new_words
+
+
+# Remove Punctuation , then Re-Plot Frequency Graph
+
+def remove_punctuation(words):
+    """Remove punctuation from list of tokenized words"""
+    new_words = []
+    for word in words:
+        new_word = re.sub(r'[^\w\s]', '', word)
+        if new_word != '':
+            new_words.append(new_word)
+    return new_words
+
+# Replace Numbers with Textual Representations
+
+
+def replace_numbers(words):
+    """Replace all interger occurrences in list of tokenized words with textual representation"""
+    p = inflect.engine()
+    new_words = []
+    for word in words:
+        if word.isdigit():
+            new_word = p.number_to_words(word)
+            new_words.append(new_word)
+        else:
+            new_words.append(word)
+    return new_words
+
+
+def remove_stopwords(words):
+    """Remove stop words from list of tokenized words"""
+    new_words = []
+    for word in words:
+        if word not in stopwords.words('english'):
+            new_words.append(word)
+    return new_words
+
+
+def stem_words(words):
+    """Stem words in a list of tokenized words"""
+    stemmer = LancasterStemmer()
+    stems = []
+    for word in words:
+        stem = stemmer.stem(word)
+        stems.append(stem)
+    return stems
+
+
+def lemmatize_verbs(words):
+    """Lemmatize verbs in a list of tokenized words"""
+    lemmatizer = WordNetLemmatizer()
+    lemmas = []
+    for word in words:
+        lemma = lemmatizer.lemmatize(word, pos='v')
+        lemmas.append(lemma)
+    return lemmas
+
+
+def normalize(words):
+    words = remove_non_ascii(words)
+    words = to_lowercase(words)
+    words = remove_punctuation(words)
+    words = replace_numbers(words)
+    words = remove_stopwords(words)
+    return words
+
+
+words = normalize(flat_word_token)
+print("Length of words list: ", len(words))
+
+
+##### Find Frequency Distribution ######
 
 # Find frequency of words
-fdist_word = FreqDist(flat_word_token)
-
-# Find frequency of sentence
-fdist_sent = FreqDist(flat_sent_token)
-
-# fdist_word.most_common(50)
+fdist_word = FreqDist(words)
+fdist_word.most_common(50)
 
 # Plot Frequency Graph
-
-# fdist_word.plot(50)
-# fdist_sent.most_common(10)
+fdist_word.plot(50)
