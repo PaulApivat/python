7a0284b5f4e45fcb13938eccbfac63827f0dca27
paulapivat <paul.apivat@gmail.com>
2021-01-26 14:52:37 +0700

    add fb_sentiment.R

---
+++ py4e/json/fb_sentiment.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+# load data
+df <- read_csv("sent_sentiment.csv")       # 3194
+df2 <- read_csv('sent_sentiment_2.csv')    # 12776
+
+# create row column for df
+df1 <- df %>%
+    mutate(row = row_number())
+
+# long-to-wide for df2
+# note: create a unique identifier for each label then use pivot_wider
+# source: https://stackoverflow.com/questions/58837773/pivot-wider-issue-values-in-values-from-are-not-uniquely-identified-output-w
+
+df3 <- df2 %>%
+    group_by(label) %>%
+    mutate(row = row_number()) %>%
+    pivot_wider(names_from = label, values_from = values) %>%
+    left_join(df1, by = 'row') %>%
+    select(row, sentence, neg:compound, numbers) 
+
+# df3 Overall summary 
+# neg 96.561
+# neu 2526.149
+# pos 568.288
+
