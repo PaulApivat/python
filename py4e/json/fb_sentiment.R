4e2b4c809ab1c5ec33334d0e3e3d34b66644cf42
paulapivat <paul.apivat@gmail.com>
2021-01-27 20:32:47 +0700

    finalized positive and negative line charts

--- py4e/json/fb_sentiment.R
+++ py4e/json/fb_sentiment.R
@@ -44,21 +44,78 @@ df3 %>%
         caption = "Data & Visualization: @paulapivat"
     )
 
-# just negative or positive
+# Line Chart: positive
 df3 %>%
     select(row:pos) %>%
     pivot_longer(cols = neg:pos, names_to = 'polarity', values_to = 'value') %>%
     filter(polarity=='pos') %>%
     ggplot(aes(x = row, y = value)) +
-    geom_line()
+    geom_line(color = "#69b3a2") +
+    theme_minimal() +
+    labs(
+        title = "Positivity Scores: Facebook Post 2006 - 2020",
+        subtitle = "Using Python NLTK Vader Library",
+        caption = "Data & Visualization: @paulapivat",
+        x = "Sentences",
+        y = "Positivity Score"
+    )
+
+# Line Chart: negative
+df3 %>%
+    select(row:pos) %>%
+    pivot_longer(cols = neg:pos, names_to = 'polarity', values_to = 'value') %>%
+    filter(polarity=='neg') %>%
+    ggplot(aes(x = row, y = value)) +
+    geom_line(color = "#404080") +
+    theme_minimal()+
+    labs(
+        title = "Negativity Scores: Facebook Post 2006 - 2020",
+        subtitle = "Using Python NLTK Vader Library",
+        caption = "Data & Visualization: @paulapivat",
+        x = "Sentences",
+        y = "Negativity Score"
+    )
+
+
+
+# Density is harder to interpret
 
 # density plot: neg, neu or pos
 df3 %>%
     select(row:pos) %>%
     pivot_longer(cols = neg:pos, names_to = 'polarity', values_to = 'value') %>%
-    filter(polarity=='neu') %>%
+    filter(polarity=='pos') %>%
     ggplot(aes(x = value)) +
     geom_density(fill="#69b3a2", color="#e9ecef", alpha=0.8)
 
-# dou
+# double sided density
+df3 %>%
+    select(row:pos) %>%
+    #pivot_longer(cols = neg:pos, names_to = 'polarity', values_to = 'value') %>%
+    ggplot(aes(x=row))+
+    geom_density(aes(x=neg, y = ..density..), fill="#69b3a2")+
+    geom_density(aes(x=pos, y = -..density..), fill= "#404080")
 
+# Histogram: Positive
+df3 %>%
+    select(row:pos) %>%
+    pivot_longer(cols = neg:pos, names_to = 'polarity', values_to = 'value') %>%
+    filter(polarity=='pos') %>%
+    filter(value > 0) %>%
+    ggplot(aes(x = value)) +
+    geom_histogram(fill="#69b3a2")
+
+# Histogram: Negative
+df3 %>%
+    select(row:pos) %>%
+    pivot_longer(cols = neg:pos, names_to = 'polarity', values_to = 'value') %>%
+    filter(polarity=='neg') %>%
+    filter(value > 0) %>%
+    ggplot(aes(x = value)) +
+    geom_histogram(fill="#404080")
+
+
+
+
+sum(df3$neg)
+sum(df3$pos)
