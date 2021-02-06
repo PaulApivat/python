05a807764115e5f36e01f64260c82e4699efd10d
paulapivat <paul.apivat@gmail.com>
2021-02-06 20:16:35 +0700

    add log10 scale to make chart more interpretable

--- py4e/json/crypto51.R
+++ py4e/json/crypto51.R
@@ -44,3 +44,34 @@ p <- df %>%
 
  
 ggMarginal(p, type = "histogram") 
+
+
+# Bubble Plot
+df %>%
+    slice(1:38) %>% 
+    select(name, market_cap, hash_rate, attack_hourly_cost) %>%
+    mutate(attack_hourly_cost = as.numeric(attack_hourly_cost)) %>%
+    # consider removing Handshake
+    filter(name != 'Bitcoin' & name != 'Ethereum' & name != 'Litecoin' & name != 'Handshake') %>%
+    # remove 0 market cap
+    filter(market_cap > 0) %>%
+    ggplot(aes(x = market_cap, y = attack_hourly_cost, size = market_cap)) +
+    geom_point(alpha = 0.7)
+
+
+# Remove coins with no marketcap
+# segment by <M, M or B
+df %>%
+    slice(1:38) %>% 
+    ggplot(aes(x=reorder(symbol, market_cap), y=market_cap, color = symbol)) +
+    geom_point(aes(size = market_cap)) +
+    geom_segment(aes(xend = symbol, yend = 0, color = symbol), size = 1) +
+    theme(
+        legend.position = 'none',
+        axis.text.x = element_text(angle = 45, hjust = 1)
+    ) +
+    scale_y_log10(labels = scales::comma)
+    
+
+
+
