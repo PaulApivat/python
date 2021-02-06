e1451e728c25ea591f15b166b44be08d5dfd72ad
paulapivat <paul.apivat@gmail.com>
2021-02-06 15:46:09 +0700

    modify crypto51.py and created csv from pandas dataframe

--- py4e/json/crypto51.py
+++ py4e/json/crypto51.py
@@ -1,10 +1,38 @@
 # coding: utf-8
+import pandas as pd
 import json
 import requests
 
 r = requests.get('https://api.crypto51.app/coins.json')
 
-print(r.json())
 type(r.json())
+
 dct = dict()
+
 dct = r.json()
+
+print(dct)
+
+# exploring dct
+
+# dict
+type(dct)
+
+# loop through:
+# last_updated
+# coins
+for x, y in dct.items():
+    print(x)
+
+# list
+type(dct['coins'])
+len(dct['coins'])  # 57 dictionaries in side this list
+
+# convert list of 57 dictionaries into a pandas dataframe
+
+
+df = pd.DataFrame.from_dict(dct['coins'])
+
+df.head()
+
+# df.to_csv('crypto51.csv', index=False)
