bce3ae994d4e93689c05abfa868285bab25250f4
paulapivat <paul.apivat@gmail.com>
2021-02-21 00:39:01 +0700

    add noodle dishes table data

--- py4e/thai_dish/thai_dish_wiki2.py
+++ py4e/thai_dish/thai_dish_wiki2.py
@@ -28,12 +28,90 @@ print(all_tables)
 
 
 # Find all table headers in the Last table
-all_tables[15].findAll('th')
+all_tables[0].findAll('th')
 
 # grabbing text from Beautiful Soup
-for item in all_tables[15].findAll('th'):
+for item in all_tables[0].findAll('th'):
     print(item.text, item.next_sibling)
 
 # saving all table header in a list
 header = [item.text.rstrip() for item in all_tables[15].findAll('th')]
 print(header)
+
+# Instead of grabbing all individual cells, grab all rows
+all_tables[0].findAll('tr')
+
+
+# Rice dishes (individual)
+a1 = []
+a2 = []
+a3 = []
+a4 = []
+a5 = []
+a6 = []
+
+# all_tables[0] - loop through to create six columns
+for row in all_tables[0].findAll('tr'):
+    cells = row.findAll('td')
+    if len(cells) == 6:
+        a1.append(cells[0].find(text=True))
+        a2.append(cells[1].find(text=True))
+        a3.append(cells[2].find(text=True))
+        a4.append(cells[3].find(text=True))
+        a5.append(cells[4].find(text=True))
+        a6.append(cells[5].find(text=True).rstrip())  # ignore italics
+
+# create dictionary
+a_d = dict([(x, 0) for x in header])
+
+# append dictionary with corresponding data list
+a_d['Thai name'] = a1
+a_d['Thai script'] = a2
+a_d['English name'] = a3
+a_d['Image'] = a4
+a_d['Region'] = a5
+a_d['Description'] = a6
+
+# turn dict into dataframe
+a_d_df_table = pd.DataFrame(a_d)
+
+# print top 5 records of first table
+a_d_df_table.head(5)
+
+#
+
+# Noodle dishes (individual)
+b1 = []
+b2 = []
+b3 = []
+b4 = []
+b5 = []
+b6 = []
+
+# all_tables[1] - loop through to create six columns
+for row in all_tables[1].findAll('tr'):
+    cells = row.findAll('td')
+    if len(cells) == 6:
+        b1.append(cells[0].find(text=True))
+        b2.append(cells[1].find(text=True))
+        b3.append(cells[2].find(text=True))
+        b4.append(cells[3].find(text=True))
+        b5.append(cells[4].find(text=True))
+        b6.append(cells[5].find(text=True).rstrip())  # ignore italics
+
+# create dictionary
+b_d = dict([(x, 0) for x in header])
+
+# append dictionary with corresponding data list
+b_d['Thai name'] = b1
+b_d['Thai script'] = b2
+b_d['English name'] = b3
+b_d['Image'] = b4
+b_d['Region'] = b5
+b_d['Description'] = b6
+
+# turn dict into dataframe
+b_d_df_table = pd.DataFrame(b_d)
+
+# print top 5 records of first table
+b_d_df_table.head(5)
