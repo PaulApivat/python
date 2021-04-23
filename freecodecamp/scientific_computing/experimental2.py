6eff6d262e9931379999ec42d4ed47f33eb4b257
paulapivat <paul.apivat@gmail.com>
2021-04-23 22:12:08 +0700

    update experimental2.py

--- freecodecamp/scientific_computing/experimental2.py
+++ freecodecamp/scientific_computing/experimental2.py
@@ -41,19 +41,50 @@ def is_correct_format(problems):
 
 def arithmetic_arranger(problems, solve=False):
     if is_correct_format(problems):
-        # arranged_problems = ""
-        dict = {"+": operator.add, "-": operator.sub}
+        arranged_problems = ""
+        first = [*range(len(problems))]
+        operator = [*range(len(problems))]
+        second = [*range(len(problems))]
+        #dict = {"+": operator.add, "-": operator.sub}
+        x = 0
         for prob in problems:
-            prob_split = prob.split()
-            first = prob_split[0]
-            second = prob_split[2]
-            sign = prob_split[1]
-            if(len(first) > len(second)):
-                line = "-" * (len(first) + 2)
+            first[x], operator[x], second[x] = prob.split()
+            x += 1
+        # first line
+        for i in range(len(problems)):
+            len_first = len(str(int(first[i])))
+            len_second = len(str(int(second[i])))
+            if len_first > len_second:
+                arranged_problems += str(" " * (len_first -
+                                                len(str(first[i])) + 2) + first[i] + " "*4)
             else:
-                line = "-" * (len(second) + 2)
-            arranged_problems = [f"{first:>{len(line) + 1 }}", '\n', sign,
-                                 f"{second:>{len(line) - 2 }}", '\n', line, '\n']
+                arranged_problems += str(" " * (len_second -
+                                                len(str(first[i])) + 2) + first[i] + " "*4)
+        arranged_problems += str("\n")
+
+        # second line
+        for i in range(len(problems)):
+            len_first = len(str(int(first[i])))
+            len_second = len(str(int(second[i])))
+            if len_first > len_second:
+                arranged_problems += str(operator[i] + " " * (len_first -
+                                                              len(str(second[i])) + 1) + second[i] + " "*4)
+            else:
+                arranged_problems += str(operator[i] + " " * (len_second -
+                                                              len(str(second[i])) + 1) + second[i] + " "*4)
+        arranged_problems += str("\n")
+        print(arranged_problems)
+
+        # third line
+        for i in range(len(problems)):
+            len_first = len(str(int(first[i])))
+            len_second = len(str(int(second[i])))
+            print("len_first:", len_first, "len_second:", len_second)
+            if len_first > len_second:
+                arranged_problems += str("--" + "-" * len_first + "    ")
+            else:
+                arranged_problems += str("--" + "-" * len_second + "    ")
+        arranged_problems += str("\n")
 
         return print(' '.join(arranged_problems))
 
