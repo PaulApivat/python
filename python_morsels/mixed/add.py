a02587702d99b3e2d22ef320cccde14efb2366d4
Paul Apivat <paulapivat@Pauls-MacBook-Pro.local>
2022-07-26 11:23:43 +0700

    add python_morsels directory and first exercise

---
+++ python_morsels/mixed/add.py
@@ -0,0 +1,4 @@
+
+# create function that accepts two lists-of-lists of numbers and returns one lists-of-list 
+# with each of the corresponding numbers in the two given lists-of-lists added together
+
