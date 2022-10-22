958ef0ef20812894818fcdb224ffcffc911c35f4
Paul <paul@Pauls-MacBook-Air.local>
2022-10-22 23:02:20 +0700

    update loops2.py

--- books/selftaught/loops2.py
+++ books/selftaught/loops2.py
@@ -7,3 +7,29 @@ for n in range(25,51):
 
 for (i, s) in enumerate(shows, start=1):
     print(i, s)
+
+
+
+numbers = ['1','8','7']
+n = 0
+while True:
+    answer = input("Guess the number or type q to quit: ")
+    if answer == 'q':
+        break 
+    elif answer in numbers:
+        print("You guessed correctly!")
+        break
+    else:
+        print("Try again!")
+
+# nested loops
+list1 = [8, 19, 148, 4]
+list2 = [9, 1, 33, 83]
+list3 = list()
+
+for i in list1:
+    for j in list2:
+        a = i*j
+        list3.append(a)
+
+print(list3)
