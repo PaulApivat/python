eebb9f264010c27af52a72c59462b08168d9d037
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-06-14 11:53:44 +0700

    update queries and tool functions for chroma_retrieval_mvp

--- random/chroma_retrieval_mvp.py
+++ random/chroma_retrieval_mvp.py
@@ -20,19 +20,28 @@ def replace_ticker(queries, ticker: str) -> list:
 # User queries - batch single queries into a list; split across 2 assets - ETH & BTC
 assets = ["ETH", "BTC"]
 
+# Q1-3: Price Changes
+# Q4-5: Volume
+# Q6: RSI
+# Q7-9: Market Cap
+# Q10: Market Dominance
+# Q11-13: News
 user_queries = [
-    "What's the price trend of {ticker} for the past 30 days?",
-    "What are the biggest price changes of {ticker} in the last 7 days?",
-    "How quickly has {ticker} price moved over the past 14 days?",
+    "What are the biggest price changes of {ticker} in the last 30 days?", 
+    "How quickly has {ticker} price moved over the past 30 days?",
+    "How volatile is {ticker} trading in the past month?",
+
     "What is the level of {ticker} buying over the past 30 days?",
     "What is the level of {ticker} selling over the past 30 days",
+
     "How does the level of {ticker} buying compare to the level of {ticker} selling for the past month?",
-    "How volatile is {ticker} trading in the past month?",
-    "What percentage of {ticker}'s total supply is already in circulation?",
-    "What is the current total dollar market value of {ticker}?"
+  
+    "What is the current total dollar market value of {ticker}?",
     "Has {ticker} reached its all-time high recently?",
     "How close is {ticker} to its ATL?",
+
     "How does the current total dollar market value of {ticker} compare to other projects?",
+
     "Are there any upcoming major vesting events for {ticker}?",
     "Are there any updates from the core developers for {ticker}?",
     "Are the market participants bullish or bearish for {ticker}?"
@@ -93,3 +102,45 @@ for queries in all_queries_assets:
         user_and_llm_queries.append({"query": queries[index], "expansion": eq})
 
 pprint(user_and_llm_queries)
+
+# Revised Questions to reflect MVP Engineering Spec
+# Price Changes, RSI, ETH, Volume, Marketcap, Market Dominance, News
+# create helper functions to turn Openai-Expanded questions (intention)
+# into individual tools
+
+
+# price change includes magnitude, speed and volatility of price changes. 
+# note: combining 3 metrics into 1 implies 3 user queries should be matched to this one function
+def price_change(ticker: str) -> str:
+    return f"""Name: {ticker} Price Change ({ticker}PC)
+
+    Description: The ticker Price Change, or {ticker}PC, measures the biggest price changes of the ${ticker} token over the past 30 days including highest and lowest values. 
+    This metric should provide insight into range of price movements for ${ticker} over the past 30 days; this could be percentage change between highest and lowest values.
+    Finally, this indicator shows how fast prices of {ticker} has changed over the past 30 days; this could be measured as percentage change over that time period. 
+
+    When to use: Use {ticker}PC to get a better understanding of the biggest price changes of {ticker} in the past 30 days.
+    {ticker}PC may also indicate how quickly {ticker} price has moved. 
+    Finally, this data can be used to understand the risk associated with investing in ${ticker}.
+    """
+
+def volume(ticker: str) -> str:
+    return f"""
+    """
+
+
+def rsi(ticker: str) -> str:
+    return f"""
+    """
+
+def market_cap(ticker: str) -> str:
+    return f"""
+    """
+
+def market_dom(ticker: str) -> str:
+    return f"""
+    """
+
+def news(ticker: str) -> str:
+    return f"""
+    """
+
