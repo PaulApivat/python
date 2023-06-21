5be92f98a1bbf8871eab711943ba9f7e8d219146
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-06-21 22:39:59 +0700

    add pythonic solution to recursively iterating through nested dictionary

--- random/tree_traversal.py
+++ random/tree_traversal.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 
+
 # nested dictionary
 root = {'data': 'A', 'children': [{'data': 'B', 'children': [{'data': 'D', 'children': []}]}, {'data': 'C', 'children': 
 [{'data': 'E', 'children': [{'data': 'G', 'children': []},
@@ -38,6 +39,7 @@ D1={1: {2: {3: 4, 5: 6}, 3: {4: 5, 6: 7}}, 2: {3: {4: 5}, 4: {6: 7}}}
 def iterdict(node):
     for key, value in node.items():
         if isinstance(value, dict):
+            print(key)
             iterdict(value)
         else:
             print(key, ":", value)
@@ -46,3 +48,23 @@ print("\n")
 print("------- iterdict -------")
 iterdict(root)
 iterdict(D1)
+
+
+# Most pythonic and easily understandable
+def recursive_items(dictionary):
+    for key, value in dictionary.items():
+        if type(value) is dict:
+            yield (key, value)
+            yield from recursive_items(value)
+        else:
+            yield (key, value)
+
+print("\n")
+print("------- recursive_items on root-------")
+for key, value in recursive_items(root):
+    print(key, value)
+
+print("\n")
+print("------- recursive_items on D1 -------")
+for key, value in recursive_items(D1):
+    print(key, value)
