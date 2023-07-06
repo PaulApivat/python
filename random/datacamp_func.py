d9b6d568b2d5f93e69339c44a86a9f24db5aa750
@paulapivat <4058461+PaulApivat@users.noreply.github.com>
2023-07-06 09:09:33 +0700

    update datacamp_func.py

--- random/datacamp_func.py
+++ random/datacamp_func.py
@@ -23,3 +23,46 @@ def shopping_list(item):
 pricing = shopping_list('apple')
 print("\n")
 print("Price is {}".format(pricing(10)))
+
+# ------------- Writing Functions in Python -------------- #
+
+def multiply(a):
+    return a * 10
+
+def subtract(a):
+    return a - 10
+
+my_functions = [multiply, subtract, print]
+
+print(my_functions[1](123))
+
+def groceries_list(my_list = []):
+    my_list.append("banana")
+    return my_list
+
+print("First groceries_list: ", groceries_list())
+print("Second groceries_list: ", groceries_list())
+
+
+"""
+def add_hello(func):
+
+    def inner():
+        func()
+    return inner
+
+@add_hello
+def simple(a, b):
+    print(a*b)
+
+print(simple(12,34))
+"""
+
+x = 32
+def my_func():
+    z = x
+    return z
+try:
+    print(my_func)
+except NameError as e:
+    print(e)
